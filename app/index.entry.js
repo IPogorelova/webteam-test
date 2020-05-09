c76e43e4de81fd4996a553bb436e0275b0c93dd8
IPogorelova <pisseversk@mail.ru>
2020-05-09 19:09:48 +0300

    add layout and styles for hero

--- app/index.entry.js
+++ app/index.entry.js
@@ -2,9 +2,12 @@ import 'reset-css';
 import './exterrnal-libs/layout/index.css'
 import './index.entry.pcss';
 import "./exterrnal-libs/breakpoints/index.css";
-import "./src/components/global.pcss";
 import "./src/components/header/header.pcss";
+import "./src/components/header/header.pcss";
+import "./src/components/hero/hero.pcss";
+
 import mobileNav from './src/components/header/header'
+
 
 document.addEventListener('DOMContentLoaded', () => {
   mobileNav()
